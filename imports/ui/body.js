7f9f97d96472d04ab70f9bfe6afc2d71aeedb0b7
Otsuki Hitoshi <hitochan777@gmail.com>
2018-11-08 20:15:55 +0900

    Forms and events

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,7 +4,28 @@ import { Tasks } from '../api/tasks.js'
 import './body.html'
 
 Template.body.helpers({
-    tasks() {
-        return Tasks.find({});
-    },
+  tasks() {
+    // Show newest tasks at the top
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
 });
